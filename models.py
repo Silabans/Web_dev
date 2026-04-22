9f925f63630f5847ee33f59c477d5923b468e517
Matthew Zimra Yedija Silaban <matthew.yedija@gmail.com>
2026-04-22 23:16:47 +0800

    complete the first version of the models file

--- models.py
+++ models.py
@@ -1,16 +1,26 @@
-import sqlalchemy as sa
-from sqlalchemy import orm
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship, DeclarativeBase
 from typing import List
 
-class Base(orm.DeclarativeBase):
+class Base(DeclarativeBase):
     pass
 
 class User(Base):
-    username: orm.Mapped[str] = orm.mapped_column(unique=True, nullable=False)
-    password: orm.Mapped[str] = orm.mapped_column(nullable=False)
-    id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
+    __tablename__ = "user_account"
 
-    tasks: orm.Mapped[List["Task"]] = orm.relationship(back_populate="owner")
+    username: Mapped[str] = mapped_column(unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    tasks: Mapped[List["Task"]] = relationship(back_populates="owner")
 
 
 class Task(Base):
+    __tablename__ = "task"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    
+    user_id: Mapped[int] = mapped_column(ForeignKey("user_account.id"))
+    content: Mapped[str] = mapped_column(nullable=False)
+    priority: Mapped[int] = mapped_column(nullable=True)
+
+    owner: Mapped["User"] = relationship(back_populates="tasks")
