9a24143c9edb4b0e78c1f88be290f4c2ed2e8835
Matthew Zimra Yedija Silaban <matthew.yedija@gmail.com>
2026-04-26 16:43:59 +0800

    add a new due date attribute to the task class

--- models.py
+++ models.py
@@ -20,8 +20,20 @@ class Task(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     
     user_id: Mapped[int] = mapped_column(ForeignKey("user_account.id"))
-    content: Mapped[str] = mapped_column(nullable=False)
-    priority: Mapped[int] = mapped_column(nullable=True)
-    isCompleted: Mapped[bool] = mapped_column()
+    content: Mapped[str] = mapped_column(nullable=False) # stores the description ofthe task
+    priority: Mapped[int] = mapped_column(nullable=True) # stores 1, 2, 3 (priority values)
+    due_date: Mapped[str] = mapped_column(nullable=True) # stores the due date
+    isCompleted: Mapped[bool] = mapped_column(default=False) # marks the completion of the task
 
-    owner: Mapped["User"] = relationship(back_populates="tasks")
+    @property
+    def priority_label(self):
+        """This function converts the numerical value of the task's priority into its word form."""
+
+        # The dictionary translates the priority values into their word forms for display
+        mapping = {1: "Low", 2: "Medium", 3: "High"}
+
+        # This takes the number from the priority attribute and returns its word form
+        # from the mapping dictionary. If the numbers are not 1, 2 or 3, it returns "Not Set"
+        return mapping.get(self.priority, "Not Set")
+
+    owner: Mapped[User] = relationship(back_populates="tasks")
